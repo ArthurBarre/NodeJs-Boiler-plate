7b2482378b38678d5032f2d3b522a0539199bb0d
ArthurBArre <arthur.barre@hetic.net>
2019-05-15 11:55:11 +0200

    Init mysql connection

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -4,18 +4,83 @@ congig routes modules
 */
 const express = require('express');
 const router = express.Router();
-//
+const mysql = require('mysql');
 
-/*
-Define routes
-*/
-// Home
-router.get('/', (req, res) => {
-  res.json({ msg: 'Hello API', error: null });
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  port: 8889,
+  database: 'Node-BoilerPlate'
 });
 
+
+
+
 /*
-export router's modules
+Define Crud
+*/
+// CRUD: Create
+router.post('/article', (req, res) => {
+
+  // Verify if the title and the content are contain in the client request
+
+  if (req.body && req.body.title.length > 0 && req.body.content.length > 0) {
+
+    //DB Connection
+    connection.connect();
+
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+      if (error) {
+        res.json({ msg: 'Error create', err: error });
+      }
+      else {
+        res.json({ msg: 'Create' });
+      }
+    });
+
+    res.json({
+      msg: 'Create',
+      data: req.body
+    });
+  }
+  else {
+    res.json({
+      msg: 'Create',
+      error: 'No data'
+    });
+  }
+});
+
+// CRUD: Read
+router.get('/article/:id', (req, res) => {
+  res.json({
+    msg: 'Read all',
+    error: null
+  });
+});
+
+// CRUD: Update
+router.put('/article/:id', (req, res) => {
+  res.json({
+    msg: 'Update one by ID',
+    error: null
+  });
+});
+
+// CRUD: Delete
+router.delete('/article/:id', (req, res) => {
+  res.json({
+    msg: 'Delete one by ID',
+    error: null
+  });
+});
+//
+
+
+/*
+Export router's modules
 */
 module.exports = router;
 //
--- server.js
+++ server.js
@@ -1,32 +1,43 @@
 /*
-import server modules
+Importer les composants serveur
 */
-
-//Class
+// Class
 require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 // Modules serveur
 const frontRoutes = require('./routes/front.routes');
 const apiRoutes = require('./routes/api.routes');
-/*
-Server configuration
-*/
+//
 
-//Define the server's variables
+/*
+Configuration du serveur
+*/
+// Définir les variables serveur
 const server = express();
 const port = process.env.PORT;
 
-
-server.set('views', __dirname + '/www');
-
-server.use(express.static(path.join(__dirname, 'www')));
-
+// Configuration du moteur de rendu
 server.set('view engine', 'ejs');
 
-server.use('/api', apiRoutes)
-server.use('/', frontRoutes);
+// Définition du dossier static du client
+server.set('views', __dirname + '/www');
+server.use(express.static(path.join(__dirname, 'www')));
 
+// Configurration de body-parser
+server.use(bodyParser.json({ limit: '10mb' }));
+server.use(bodyParser.urlencoded({ extended: true }));
+
+// Utilisation des routeurs
+server.use('/api', apiRoutes);
+server.use('/', frontRoutes);
+//
+
+/* 
+Lancer le serveur
+*/
 server.listen(port, () => console.log(`Server is launched on poort ${port}`))
+//
