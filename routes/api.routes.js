f49cc2c80a576646eb15d17b9f4b1330501efe31
ArthurBArre <arthur.barre@hetic.net>
2019-05-15 12:06:40 +0200

    Init mysql connection

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,86 +1,84 @@
 
 /*
-congig routes modules
+Configurer le module de route
 */
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
+//
 
-var connection = mysql.createConnection({
+/* 
+MySql Config
+*/
+const mysql = require('mysql');
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
   port: 8889,
-  database: 'Node-BoilerPlate'
+  database: 'node-boiler-plate'
 });
-
-
-
+//
 
 /*
-Define Crud
+Define CRUD
 */
 // CRUD: Create
 router.post('/article', (req, res) => {
 
-  // Verify if the title and the content are contain in the client request
-
+  /* 
+test if title and content exist
+  */
   if (req.body && req.body.title.length > 0 && req.body.content.length > 0) {
 
-    //DB Connection
+    // Connexion de la BDD
     connection.connect();
 
+    // Define item
+    const item = { title: req.body.title, content: req.body.content };
 
-    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    // Send MySql data
+    connection.query('INSERT INTO post SET ?', item, (error, results, fields) => {
       if (error) {
-        res.json({ msg: 'Error create', err: error });
+        res.json({ msg: 'Error create', err: error })
       }
       else {
-        res.json({ msg: 'Create' });
+        res.json({ msg: 'Create', data: results })
       }
     });
 
-    res.json({
-      msg: 'Create',
-      data: req.body
-    });
+    // Close connection
+    connection.end();
   }
+
   else {
-    res.json({
-      msg: 'Create',
-      error: 'No data'
-    });
+    res.json({ msg: 'Create', error: 'No data' })
   }
 });
 
 // CRUD: Read
+router.get('/article', (req, res) => {
+  res.json({ msg: 'Read ALL', error: null })
+});
+
+// CRUD: Read
 router.get('/article/:id', (req, res) => {
-  res.json({
-    msg: 'Read all',
-    error: null
-  });
+  res.json({ msg: 'Read one by ID', error: null })
 });
 
 // CRUD: Update
 router.put('/article/:id', (req, res) => {
-  res.json({
-    msg: 'Update one by ID',
-    error: null
-  });
+  res.json({ msg: 'Update one by ID', error: null })
 });
 
 // CRUD: Delete
 router.delete('/article/:id', (req, res) => {
-  res.json({
-    msg: 'Delete one by ID',
-    error: null
-  });
+  res.json({ msg: 'Delete one by ID', error: null })
 });
 //
 
 
 /*
-Export router's modules
+Export Router Module
 */
 module.exports = router;
 //
